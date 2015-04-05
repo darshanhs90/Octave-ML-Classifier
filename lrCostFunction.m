42d8158419cf42aaa487ef6d2ed38906661cd764
darshanhs90 <hsdars@gmail.com>
2015-04-05 14:22:57 +0530

    Ver 1.0

---
+++ lrCostFunction.m
@@ -0,0 +1,91 @@
+function [J, grad] = lrCostFunction(theta, X, y, lambda)
+%LRCOSTFUNCTION Compute cost and gradient for logistic regression with 
+%regularization
+%   J = LRCOSTFUNCTION(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly
+
+J = 0;
+grad = zeros(size(theta));
+sum1=0;
+s=sigmoid(X*theta);
+
+temp1=(-y.*log(s))-((1-y).*log(1-s));
+J=temp1/m;
+J=sum(J(1:end));
+%size(J);
+J=J+sum1;
+%Grad calculation
+
+%fprintf('theta')
+%size(theta)
+%theta
+%fprintf('X')
+%size(X)
+%fprintf('Y')
+%size(y)
+%fprintf('S')
+%size(s)
+%J
+%grad(:)
+
+grad1 = zeros(size(theta));
+
+t=(lambda)*(sum(theta(2:end).^2));
+t=t/(2*m);
+
+J=J+t;
+
+%Grad calculation
+grad=X'*(s-y);
+grad=grad./m;
+temp=grad(1);
+grad=grad+((theta*lambda)./m);
+grad(1)=temp;
+%grad
+
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Hint: The computation of the cost function and gradients can be
+%       efficiently vectorized. For example, consider the computation
+%
+%           sigmoid(X * theta)
+%
+%       Each row of the resulting matrix will contain the value of the
+%       prediction for that example. You can make use of this to vectorize
+%       the cost function and gradient computations. 
+%
+% Hint: When computing the gradient of the regularized cost function, 
+%       there're many possible vectorized solutions, but one solution
+%       looks like:
+%           grad = (unregularized gradient for logistic regression)
+%           temp = theta; 
+%           temp(1) = 0;   % because we don't add anything for j = 0  
+%           grad = grad + YOUR_CODE_HERE (using the temp variable)
+%
+
+
+
+
+
+
+
+
+
+
+% =============================================================
+
+grad = grad(:);
+
+end
